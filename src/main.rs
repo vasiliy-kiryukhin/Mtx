0c5f9177e7137ba2f3396e4f25f8d36281db729f
vasiliy-kiryukhin <vasiliy.kiryukhin@gmail.com>
2024-05-21 22:44:32 +0300

    dummy

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use rand::prelude::*;
 use rand::Rng;
 const CHAR_SET: [char; 5] = ['A', 'B', 'C', 'D', 'E'];
+const DEFAULT_CHAR : char = 'Z';
+
 struct MtxSubLine {
     length: u8,
     chars: Vec<char>,
@@ -8,13 +9,18 @@ struct MtxSubLine {
     bottom_position: i32,
 }
 impl MtxSubLine {
-    fn new(length: u8, bottom_position: i32, chars_idx: &Vec<i32>) -> Self {
+    fn new(length: u8, bottom_position: i32, chars_idx: &Vec<usize>) -> Self {
+        let subline_chars: Vec<char> = chars_idx.iter().map(|x| *CHAR_SET.get(*x).unwrap()).collect();
         MtxSubLine {
             length,
-            chars: vec![],
+            chars: subline_chars,
             top_position: bottom_position + length as i32,
             bottom_position,
         }
+    }
+    fn move_down(&mut self) {
+        self.bottom_position +=1;
+        self.top_position +=1;
     }
 }
 struct MtxLine<'a> {
@@ -23,6 +29,8 @@ struct MtxLine<'a> {
 fn main() {
     let mut rng = rand::thread_rng();
     let len = rng.gen_range(5..10);
+    let chars_idx : Vec<usize> = (0..3).map(|_| rng.gen_range(0..4)).collect();
 
+    let subLine = MtxSubLine::new(len, -3, &chars);
     let line = MtxLine { lines: vec![] };
 }
